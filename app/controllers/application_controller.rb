cb65601f33a724e36f136e8b07f3df958c246b3d
Evan Brown <evan@factris.com>
2022-10-03 16:47:29 +0100

    Update authorisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :doorkeeper_authorize!
   attr_reader :current_user
 
   include Pundit
+
+  def doorkeeper_unauthorized_render_options(*)
+    { json: { errors: [{ title: 'Not authorised - please log in' }] } }
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
   def make_index(cls, collection)
     cls.new request.path, ActionController::Parameters.new(request.query_parameters), collection
   end
