60150965289a3f3846f1760dcd31becc95d46add
Evan Brown <evan@factris.com>
2022-10-03 17:19:13 +0100

    Update authentication yet again

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,14 +24,12 @@ RSpec.configure do |config|
       paths: {},
       components: {
         securitySchemes: {
-          songkick_api_security_schema: {
-            flows: [
-              {
-                type: :oauth2,
-                flow: 'password',
-                tokenUrl: '/oauth/token'
-              }
-            ]
+          songkick_api: {
+            flows: [{
+              type: :oauth2,
+              flow: 'password',
+              tokenUrl: '/oauth/token'
+                    }]
           }
         }
       }
