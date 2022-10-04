05dc979e5af1517c798b9a29042a8f40f8585c7a
Evan Brown <evan@factris.com>
2022-10-04 23:59:35 +0100

    Tidy up user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ApplicationRecord
 
   authenticates_with_sorcery!
 
-  has_many :access_grants,
-           class_name: 'Doorkeeper::AccessGrant',
-           foreign_key: :resource_owner_id,
-           dependent: :destroy
-
   has_many :access_tokens,
            class_name: 'Doorkeeper::AccessToken',
            foreign_key: :resource_owner_id,
